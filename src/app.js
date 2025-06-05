29f51814f2c8207df4132032c2e90ed11c6ce109
RamonMartinez01 <mafrtinezcjr1@gmail.com>
2025-06-05 09:46:21 -0700

    feat: configuración inicial para el backend con Node.js + Express + Sequelize

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const router = require('./routes');
+const errorHandler = require('./utils/errorHandler');
+require('dotenv').config();
+
+// Esta es nuestra aplicación
+const app = express();
+
+// Middlewares 
+app.use(express.json());
+app.use(helmet({
+    crossOriginResourcePolicy: false,
+}));
+app.use(cors());
+
+app.use(router);
+app.get('/', (req, res) => {
+    return res.send("Welcome to express!");
+})
+
+module.exports = app;
