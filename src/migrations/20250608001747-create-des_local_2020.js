ea556d7a07010e6a1ca0c1d4044993521e0f8c23
RamonMartinez01 <mafrtinezcjr1@gmail.com>
2025-06-08 08:27:01 -0700

    fix: corrige 'type: Sequelize' en campos de migración 'xxxx-create-des_local_2020.js'

--- src/migrations/20250608001747-create-des_local_2020.js
+++ src/migrations/20250608001747-create-des_local_2020.js
@@ -13,45 +13,123 @@ module.exports = {
           type: DataTypes.STRING,
           primaryKey: true,
         },
-        CGLOC: DataTypes.STRING,
-        PTL_0101: DataTypes.STRING,
-        TPU_0102: DataTypes.STRING,
-        ABS_0101: DataTypes.STRING,
-        ABS_0102: DataTypes.STRING,
-        ABS_0106: DataTypes.STRING,
-        ABS_0107: DataTypes.STRING,
-        ABS_0110: DataTypes.STRING,
-        COM_0101: DataTypes.STRING,
-        COM_0103: DataTypes.STRING,
-        COM_0104: DataTypes.STRING,
-        COM_0109: DataTypes.STRING,
-        SAS_0101: DataTypes.STRING,
-        SAS_0102: DataTypes.STRING,
-        SAS_0103: DataTypes.STRING,
-        TEL_0101: DataTypes.STRING,
-        TEL_0102: DataTypes.STRING,
-        TEL_0103: DataTypes.STRING,
-        TEL_0106: DataTypes.STRING,
-        AEC_0101: DataTypes.STRING,
-        AEC_0301: DataTypes.STRING,
-        AEC_0302: DataTypes.STRING,
-        AEC_0303: DataTypes.STRING,
-        AUT_0101: DataTypes.STRING,
-        AUT_0102: DataTypes.STRING,
-        AUT_0103: DataTypes.STRING,
-        AUT_0104: DataTypes.STRING,
-        CON_0101: DataTypes.STRING,
-        CON_0102: DataTypes.STRING,
-        CON_0103: DataTypes.STRING,
-        CON_0104: DataTypes.STRING,
-        FEN_0101: DataTypes.STRING,
-        FEN_0102: DataTypes.STRING,
-        FEN_0103: DataTypes.STRING,
-        FEN_0104: DataTypes.STRING,
-        FEN_0106: DataTypes.STRING,
-        CHI_0101: DataTypes.STRING,
-        CHI_0102: DataTypes.STRING,
-        PRP_0101: DataTypes.STRING,
+        CGLOC:{
+          type: Sequelize.STRING,
+         },
+        PTL_0101: {
+          type: Sequelize.STRING,
+         },
+        TPU_0102: {
+          type: Sequelize.STRING,
+         },
+        ABS_0101: {
+          type: Sequelize.STRING,
+         },
+        ABS_0102: {
+          type: Sequelize.STRING,
+         },
+        ABS_0106: {
+          type: Sequelize.STRING,
+         },
+        ABS_0107: {
+          type: Sequelize.STRING,
+         },
+        ABS_0110: {
+          type: Sequelize.STRING,
+         },
+        COM_0101: {
+          type: Sequelize.STRING,
+         },
+        COM_0103: {
+          type: Sequelize.STRING,
+         },
+        COM_0104: {
+          type: Sequelize.STRING,
+         },
+        COM_0109: {
+          type: Sequelize.STRING,
+         },
+        SAS_0101: {
+          type: Sequelize.STRING,
+         },
+        SAS_0102: {
+          type: Sequelize.STRING,
+         },
+        SAS_0103: {
+          type: Sequelize.STRING,
+         },
+        TEL_0101: {
+          type: Sequelize.STRING,
+         },
+        TEL_0102: {
+          type: Sequelize.STRING,
+         },
+        TEL_0103: {
+          type: Sequelize.STRING,
+         },
+        TEL_0106: {
+          type: Sequelize.STRING,
+         },
+        AEC_0101: {
+          type: Sequelize.STRING,
+         },
+        AEC_0301: {
+          type: Sequelize.STRING,
+         },
+        AEC_0302: {
+          type: Sequelize.STRING,
+         },
+        AEC_0303: {
+          type: Sequelize.STRING,
+         },
+        AUT_0101: {
+          type: Sequelize.STRING,
+         },
+        AUT_0102: {
+          type: Sequelize.STRING,
+         },
+        AUT_0103: {
+          type: Sequelize.STRING,
+         },
+        AUT_0104: {
+          type: Sequelize.STRING,
+         },
+        CON_0101: {
+          type: Sequelize.STRING,
+         },
+        CON_0102: {
+          type: Sequelize.STRING,
+         },
+        CON_0103: {
+          type: Sequelize.STRING,
+         },
+        CON_0104: {
+          type: Sequelize.STRING,
+         },
+        FEN_0101: {
+          type: Sequelize.STRING,
+         },
+        FEN_0102: {
+          type: Sequelize.STRING,
+         },
+        FEN_0103: {
+          type: Sequelize.STRING,
+         },
+        FEN_0104: {
+          type: Sequelize.STRING,
+         },
+        FEN_0106: {
+          type: Sequelize.STRING,
+         },
+        CHI_0101: {
+          type: Sequelize.STRING,
+         },
+        CHI_0102: {
+          type: Sequelize.STRING,
+         },
+        PRP_0101: {
+          type: Sequelize.STRING,
+         },
         ID_LOC: { 
           type: Sequelize.INTEGER,
           references: {
