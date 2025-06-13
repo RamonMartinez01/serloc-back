0e8323029e147d4de41d6e822439622fec7c0118
RamonMartinez01 <mafrtinezcjr1@gmail.com>
2025-06-13 13:42:32 -0700

    feat: Crea controladores y rutas para el issue #25. Y otras. - Crea controlador 'getMunicipiosPorClaveEntidad' con la ruta '/estados/:cve_ent/municipios' para ver todos los municipios del estado correspondiente a la CVE_ENT dada - Crea la ruta '/estados/:cve_ent/municipios/:cve_mun' para obtener UN municipio de clave CVE_MUN del estado de la clave CVE_ENT dada. - Crea controlador la ruta '/estados/:cve_ent/municipios/:cve_mun/localidades' para obtener todas las localidades de un municipio de clave CVE_MUN dentro de un estado correspondiente a la clave CVE_ENT dada.

--- src/routes/municipios.router.js
+++ src/routes/municipios.router.js
@@ -1,10 +1,13 @@
-const { getMunicipios,  getOne } = require('../controllers/municipios.controllers');
+const { getMunicipios,  getOne, getMunicipiosConLocalidades } = require('../controllers/municipios.controllers');
 const express = require('express');
 
 const municipiosRouter = express.Router();
 
 municipiosRouter.route("/municipios")
     .get(getMunicipios);  // GET all municipios
+
+municipiosRouter.route("/municipios-localidades")
+    .get(getMunicipiosConLocalidades)
 
 municipiosRouter.route("/municipios/:id")
     .get(getOne);  // GET a single municipio by ID_MUN
